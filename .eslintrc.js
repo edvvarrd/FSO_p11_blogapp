442b52802987b656ce2ea145851d2c2b17dba764
Hollow <edvvardsky@gmail.com>
2024-08-20 00:09:27 +0300

    Exercise 11.20

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,8 +21,5 @@ module.exports = {
     'object-curly-spacing': ['error', 'always'],
     'arrow-spacing': ['error', { before: true, after: true }],
     'no-console': 0,
-    'react/prop-types': 0,
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 0,
   },
 }
