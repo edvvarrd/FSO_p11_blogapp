7b91b21fe3f7dfe58a8f190bf2c6fdfa7c0fcfea
Hollow <edvvardsky@gmail.com>
2024-08-20 12:11:18 +0300

    Exercise 11.20

--- utils/config.js
+++ utils/config.js
@@ -4,7 +4,7 @@ let PORT = process.env.PORT || 3001
 let MONGODB_URI = process.env.MONGODB_URI
 
 if (process.env.NODE_ENV === 'test') {
-	MONGODB_URI = process.env.MONGODB_URI_TEST
+	MONGODB_URI = process.env.TEST_MONGODB_URI
 }
 
 module.exports = { MONGODB_URI, PORT }
